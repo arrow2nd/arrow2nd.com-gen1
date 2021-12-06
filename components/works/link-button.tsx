658f3498d2d9e990b8faa225e8eedcfc369fe171
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-06 22:52:33 +0900

    fix

--- components/works/link-button.tsx
+++ components/works/link-button.tsx
@@ -3,19 +3,15 @@ import FaIcon from 'components/common/fa-icon'
 import type { LinkContent } from 'types/cms/work'
 
 const LinkButton = ({ icon, text, href }: LinkContent): JSX.Element => (
-  <div
-    className={`m-2 px-4 md:px-6 py-1 md:py-1.5 inline-block text-natural-white bg-natural-gray hover:bg-black rounded-full transition-colors`}
+  <a
+    className={`inline-block m-2 px-4 md:px-6 py-1 md:py-1.5 text-sm md:text-base text-natural-white bg-natural-gray hover:bg-black rounded-full transition-colors`}
+    href={href}
+    target="_blank"
+    rel="noopener noreferrer"
   >
-    <a
-      className="text-sm md:text-base"
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <FaIcon name={icon[0]} />
-      <span className="ml-1.5 inline-block">{text}</span>
-    </a>
-  </div>
+    <FaIcon name={icon[0]} />
+    <span className="ml-1.5 inline-block">{text}</span>
+  </a>
 )
 
 export default LinkButton
