f95514d0277fa5325fb12cbe15954a354f175a9c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-06 16:39:18 +0900

    change: 作品タイトルの文字色

--- components/works/title.tsx
+++ components/works/title.tsx
@@ -7,7 +7,9 @@ type Props = {
 
 const Title = ({ text, description }: Props): JSX.Element => (
   <div className="mt-8 text-center">
-    <h2 className="text-xl md:text-2xl tracking-widest">{text}</h2>
+    <h2 className="text-xl md:text-2xl text-natural-black tracking-widest">
+      {text}
+    </h2>
     <p className="mt-1 text-sm md:text-base text-natural-gray">{description}</p>
     <Accent className="mt-8" />
   </div>
