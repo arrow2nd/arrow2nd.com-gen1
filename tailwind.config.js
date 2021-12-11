4a2387d60ec8e6f0800f501d71e160e474e17bf3
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-11 20:34:44 +0900

    update: tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -16,11 +18,6 @@ module.exports = {
       letterSpacing: {
         super: '.25rem'
       }
-    }
-  },
-  variants: {
-    extend: {
-      brightness: ['hover']
     }
   },
   plugins: []
